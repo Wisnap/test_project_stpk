3b7f9d00f1f084ac5ae3037c62735adc650d0c5e
Pavel Vishnevskii <wisnap@MacBook-Pro-Pavel.local>
2020-10-29 00:37:32 +0300

    fixes

--- test_product_page.py
+++ test_product_page.py
@@ -70,7 +70,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -85,7 +85,7 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page.is_not_basket_element_present()
     basket_page.should_be_empty_text_in_basket()
 
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
